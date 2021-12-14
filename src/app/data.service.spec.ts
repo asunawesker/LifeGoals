0fba8136170157d4854fdf3982d8d5f00372d05c
Irais Aguirre <asunawesker@pop-os.localdomain>
2021-12-14 06:16:02 -0600

    adding some things

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule, HttpResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
@@ -6,11 +7,39 @@ describe('DataService', () => {
   let service: DataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [
+        DataService
+      ]
+    });
     service = TestBed.inject(DataService);
   });
 
-  xit('should be created', () => {
-    expect(service).toBeTruthy();
+  it('The api should have some data', (done) => {
+    service.getGoals().subscribe(data => { 
+        const dataArray: any[] = Array.of(data);
+        expect(dataArray.length).toBeGreaterThan(0);
+        done();
+    });
   });
+
+  it('should add an album and return the artist`s name', () => {
+    const newAlbum = { album_name: 'Diamond Eyes', album_artist: 'Deftones' };
+
+    service.newGoal(newAlbum).subscribe(
+      data => expect(data.album_artist).toEqual(newAlbum.album_artist),
+      fail
+    );
+  });
+
+  it('should add an album and return the name of the album', () => {
+    const newAlbum = { album_name: 'Diamond Eyes', album_artist: 'Deftones' };
+
+    service.newGoal(newAlbum).subscribe(
+      data => expect(data.album_name).toEqual(newAlbum.album_name),
+      fail
+    );
+  });
+
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,14 +11,14 @@ import { DataService } from '../data.service';
       transition('* => *', [
         query(':enter', style({ opacity: 0 }), {optional: true}),
 
-        query(':enter', stagger('300ms', [
+        query(':enter', stagger('5ms', [
           animate('.6s ease-in', keyframes([
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1}),
           ]))]),{optional: true}), 
 
-        query(':leave', stagger('300ms', [
+        query(':leave', stagger('5ms', [
           animate('.6s ease-in', keyframes([
             style({opacity: 1, transform: 'translateY(0)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
